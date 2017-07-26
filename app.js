b60b0e9b1dee03d551912dbabf84a8e8d342213f
zy343134464 <zy343134464@163.com>
2017-07-26 16:07:18 +0800

    完成基本清单功能,数据保存在本地

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-import Vue from 'vue';
+import Vue from 'vue'
 
 var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-})    
+	el: '#app',
+	data: {
+		newTodo: '',
+		todoList: []
+	},
+	created: function(){
+	    window.onbeforeunload = ()=>{
+	      var dataString = JSON.stringify(this.todoList) 
+	      window.localStorage.setItem('myTodos', dataString)  // 在用户关闭页面前，将数据保存在 localStorage 里
+	    }
+	    var oldDataString = window.localStorage.getItem('myTodos')
+	    var oldData = JSON.parse(oldDataString)
+	    this.todoList = oldData || [] // 在用户进入页面后，立刻读取 localStorage
+  	},
+	methods: {
+		addTodo: function () {
+			this.todoList.push({
+				title: this.newTodo,
+				createdAt: new Date(),
+				done: false // 添加一个done属性
+			})
+			this.newTodo = '' // 清空
+		},
+		removeTodo: function(todo){
+      		var index = this.todoList.indexOf(todo)
+      		this.todoList.splice(index,1)
+    	}
+	}
+})   
--- bundle.js
+++ bundle.js
@@ -74,11 +74,36 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 var app = new __WEBPACK_IMPORTED_MODULE_0_vue___default.a({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-})    
+	el: '#app',
+	data: {
+		newTodo: '',
+		todoList: []
+	},
+	created: function(){
+	    window.onbeforeunload = ()=>{
+	      var dataString = JSON.stringify(this.todoList) 
+	      window.localStorage.setItem('myTodos', dataString)  // 在用户关闭页面前，将数据保存在 localStorage 里
+	    }
+	    var oldDataString = window.localStorage.getItem('myTodos')
+	    var oldData = JSON.parse(oldDataString)
+	    this.todoList = oldData || [] // 在用户进入页面后，立刻读取 localStorage
+  	},
+	methods: {
+		addTodo: function () {
+			this.todoList.push({
+				title: this.newTodo,
+				createdAt: new Date(),
+				done: false // 添加一个done属性
+			})
+			this.newTodo = '' // 清空
+		},
+		removeTodo: function(todo){
+      		var index = this.todoList.indexOf(todo)
+      		this.todoList.splice(index,1)
+    	}
+	}
+})   
+
 
 /***/ }),
 /* 1 */
